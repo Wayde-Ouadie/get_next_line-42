b5f089f95f97e777c4baf8d4c38adc0cc8f45cfa
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2023-11-27 12:49:26 +0100

    GNL -b

---
+++ get_next_line_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 11:31:16 by oel-feng          #+#    #+#             */
+/*   Updated: 2023/11/27 12:43:01 by oel-feng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_get_line(char *line)
+{
+	char	*res;
+	size_t	i;
+
+	i = 0;
+	while (line[i] && line[i] != '\n') 
+		i++;
+	if (line[i] == '\0')
+		return (NULL);
+	res = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (!res)
+	{
+		if (line)
+			free(line);
+		line = NULL;
+		return (NULL);
+	}
+	if (*res == '\0')
+	{
+		free(res);
+		res = NULL;
+	}
+	line[i + 1] = '\0';
+	return (res);
+}
+
+static char	*read_line_check(int fd, char *buff, char *line)
+{
+	int		lread;
+	char	*tmp;
+
+	lread = 1;
+	while (lread)
+	{
+		lread = read(fd, buff, BUFFER_SIZE);
+		if (lread == -1)
+			return (NULL);
+		else if (lread == 0)
+			break ;
+		buff[lread] = '\0';
+		if (!line)
+			line = ft_strdup("");
+		tmp = line;
+		line = ft_strjoin(tmp, buff);
+		free(tmp);
+		tmp = NULL;
+		if (!line)
+			return (NULL);
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line;
+	char		*tmp;
+	char		*buff;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	tmp = read_line_check(fd, buff, line);
+	free(buff);
+	buff = NULL;
+	if (!tmp)
+	{
+		if (line)
+			free(line);
+		line = NULL;
+		return (NULL);
+	}
+	line = ft_get_line(tmp);
+	return (tmp);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 11:31:11 by oel-feng          #+#    #+#             */
+/*   Updated: 2023/11/26 23:45:33 by oel-feng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	if (!str)
+		return (NULL);
+	while ((char)c != *str)
+	{
+		if (!*str)
+			return (NULL);
+		str++;
+	}
+	return ((char *)str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*res;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		res[i] = s2[j];
+		j++;
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*res;
+	size_t	i;
+	size_t	j;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		j = 0;
+	else if (ft_strlen(s) - start < len)
+		j = ft_strlen(s) - start;
+	else
+		j = len;
+	res = malloc(sizeof(char) * (j + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < j)
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*res;
+	size_t	i;
+
+	res = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		res[i] = s[i];
+		i++;
+	}
+	res[i] = 0;
+	return (res);
+}
